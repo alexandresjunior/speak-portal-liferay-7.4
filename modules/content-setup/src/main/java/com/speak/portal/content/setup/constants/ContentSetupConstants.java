83d02a3af27fc44e36f9edd6a9db63c32c51fb7b
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-01-28 19:39:03 -0300

    Create admin user upgrade process

---
+++ modules/content-setup/src/main/java/com/speak/portal/content/setup/constants/ContentSetupConstants.java
@@ -0,0 +1,23 @@
+package com.speak.portal.content.setup.constants;
+
+/**
+ * Content setup constants class.
+ * It contains all the String literals required for the upgrade processes.
+ *
+ * @author Alexandre de Souza Jr.
+ */
+public class ContentSetupConstants {
+
+	public static final String ADMIN_USER_EMAIL_ADDRESS = "admin.user@liferay.com";
+
+	public static final String PUBLIC_VIRTUAL_HOST_PROPERTY = "public.virtual.host";
+
+	public static final String SPEAK_PORTAL = "Speak Portal";
+
+	public static final String SPEAK_PORTAL_FRIENDLY_URL = "/speak";
+
+	public static final String TYPE_FRAGMENT = "fragment";
+
+	public static final String TYPE_WIDGET = "widget";
+
+}
